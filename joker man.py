460d5776aa76df67c7e480280f134072d946903c
Qwabena <100248770+Qwabena-Proxy@users.noreply.github.com>
2022-04-19 11:55:21 +0000

    Add files via upload

---
+++ joker man.py
@@ -0,0 +1,24 @@
+import pyjokes as pj
+print('''Jokes are availble in the following languages;
+*English
+*German
+*Spanish
+*Italian
+*Galician
+*Basque
+''')
+lang = input('Enter language: ').lower()
+language_dict = {
+  'english':'en',
+  'german':'de',
+  'spanish':'es',
+  'italian':'it',
+  'galician':'gl',
+  'basque':'eu'
+}
+try:
+  lang = language_dict[f'{lang}']
+  joke = pj.get_joke(language = lang , category = 'all')
+  print(joke)
+except:
+  print('Language entered is not in the list above..😏😏')
